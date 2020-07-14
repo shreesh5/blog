5fa4e7640939351137dc5e6ed2bb33e9863fe2d6
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-14 16:23:17 -0700

    Added list of blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+
+    const blogPosts = useContext(BlogContext);
+
     return (
         <View>
             <Text>Index Screen</Text>
+            <FlatList 
+                data={blogPosts}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return (
+                        <Text>{item.title}</Text>
+                    );
+                }}
+            />
         </View>
     );
 };
